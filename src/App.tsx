06086d4402b6dec028eeb1427566eeb7a7212f99
Therese <therese.holen22@gmail.com>
2023-08-16 15:50:54 +0200

    Added services and install axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import { RouterProvider} from "react-router-dom"
 import { router } from "./models/Router"
 import './App.css'
+import { IAnimals } from "./models/IAnimals";
+import { showAnimals } from "./services/animalService";
 
 function App() {
   //const [count, setCount] = useState(0)
 
+  const [animals, setAnimals] = useState<IAnimals[]>([])
+
+  const showAnimals = async () => {
+    if (animals.length ===0 ) {
+      //setAnimals(await showAnimals());
+      
+    } else {
+      return;
+    }
+    console.log(animals);
+  };
+showAnimals();
   return (
     <>
       <RouterProvider router= {router}></RouterProvider>
---
+++ src/services/animalService.ts
@@ -0,0 +1,17 @@
+import { IAnimal } from "../models/IAnimal";
+import { IAnimals } from "../models/IAnimals";
+import axios from "axios";
+
+const BASEURL = "https://animals.azurewebsites.net/api/animals";
+
+export const showAnimals = async () => {
+    const response = await axios.get<IAnimals>(BASEURL);
+
+    return response.data; 
+}
+
+export const showSelectedAnimal = async (id:number) => {
+    const response = await axios.get<IAnimal>(`${BASEURL}i=´${id}`);
+    
+    return response.data;
+}
