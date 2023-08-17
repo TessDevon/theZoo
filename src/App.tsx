35b9951fa5663985be8eef487e327c867fe3e9f1
Therese <therese.holen22@gmail.com>
2023-08-17 06:48:31 +0200

    Added first draft of my studyplan.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import { IAnimals } from "./models/IAnimals";
 import { showAnimals } from "./services/animalService";
 
+
 function App() {
   //const [count, setCount] = useState(0)
 
@@ -28,3 +29,17 @@ showAnimals();
 }
 
 export default App
+
+/** Plan att fixa till uppgiften till denna vecka:
+ * Planera upp uppgiften - KLAR
+ * Addera komponenter jag kommer behöva i projektet - KLAR
+ * Få rätt på routs och se till att de visas rätt sidor vid ändring i URLen - KLAR
+ * 
+ * Få rätt på hämtning - under process
+ * Koppla länken med djur till sidan och funktionen som hämtar - under process
+ * Spara i Localstorage - under process
+ * 
+ * Props med info till animals
+ * Skriv djuren på sidan med animals
+ * Lägg till styling. Även bakgrund med olika klasser som ska ändras utifrån tid. Matad grön, 3h gul, 4h röd.
+ */
