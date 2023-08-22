fb6bf7077e72836c91994dc5131dbf10b87383e1
Therese <therese.holen22@gmail.com>
2023-08-22 20:38:41 +0200

    Cleaned code

--- src/components/feedAnimals.ts
+++ src/components/feedAnimals.ts
@@ -2,7 +2,6 @@ import { IAnimals } from "../models/IAnimals";
 
 export const feedAnimal = (animalId: string) => {
   const feedDate = new Date().toISOString();
-  console.log(feedDate);
 
   const localStorageAnimals = JSON.parse(
     localStorage.getItem("animals") || "[]"
